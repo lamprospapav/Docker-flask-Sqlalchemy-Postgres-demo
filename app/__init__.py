2e65608061da09b3ef0a386bce299fba5f351a6f
lamprospapav <lamprospapav@gmail.com>
2019-02-07 20:40:27 +0200

    Blueprint-Home

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 #local imports
 from settings import app_settings
@@ -7,7 +8,7 @@ from settings import app_settings
 # db variable initialization
 
 db = SQLAlchemy()
-
+#login_Manager = Login_Manager()
 
 def create_app(settings_name):
     app = Flask(__name__,instance_relative_config=True)
@@ -15,10 +16,14 @@ def create_app(settings_name):
     app.config.from_pyfile('settings.py')
     db.init_app(app)
 
-    # temporary route
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
+    #from app import models
+
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
+    #login_Manager.init_app(app)
+    #login_manager.login_message = "You must be logged in to access this page."
+    #login_manager.login_view = "auth.login"
 
 
     return app
